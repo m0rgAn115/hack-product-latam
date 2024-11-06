ef09f9be91a80ba935057aebfced683714a0ea84
danielctecla <daniel.ctecla@gmail.com>
2024-11-06 16:48:12 -0600

    [plaid-connection] fix: 'RNCWebView' could not be found'

--- app/(tabs)/webview.tsx
+++ app/(tabs)/webview.tsx
@@ -1,36 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import PlaidLink from '@/components/Plaid/PlaidLink'
-import { Text } from 'react-native';
+import React from 'react';
+import { StyleSheet  } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-
-  const [linkToken, setLinkToken] = useState(null);
-
-  useEffect(() => {
-    const createLinkToken = async () => {
-      const response = await fetch('https://6vy6af7h6j.execute-api.us-east-1.amazonaws.com/Prod/api/create_link_token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await response.json();
-      setLinkToken(data.link_token); // Asigna el token de enlace recibido
-    };
-
-    createLinkToken();
-  }, []);
-
-  if (!linkToken) {
-    return <Text>Loading...</Text>; // Muestra un mensaje de carga mientras se genera el token
-  }
+const WebViewComponent: React.FC = () => {
+  const url = 'https://blog.logrocket.com/';
 
   return (
-    <PlaidLink
-      linkToken={linkToken}
-      onEvent={(event) => console.log(event)}
-      onExit={(exit) => console.log(exit)}
-      onSuccess={(success) => console.log(success.publicToken)}
-    />
-  )
-}
+      <WebView source={{ uri: url }} style={styles.webview} />
+  );
+};
+
+const styles = StyleSheet.create({
+  webview: {
+    flex: 1,
+    width: '100%',
+  },
+});
+
+export default WebViewComponent;
